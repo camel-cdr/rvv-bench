acd1fc00510a7e1d24e318154e7aad51c18b0b1a
Olaf Bernstein <camel-cdr@protonmail.com>
2023-11-15 16:40:27 +0100

    add missing LMUL=8 widening reductions

--- instructions/rvv-1.0/config.h
+++ instructions/rvv-1.0/config.h
@@ -1,12 +1,14 @@
 /* processor specific configs */
 
 // e64/e32/e16/e8 m8/m4/m2/m1
-#define T_A  0b11111111 // all
-#define T_W  0b01110111 // widen
-#define T_N  0b01110111 // narrow
-#define T_F  0b11101111 // float
-#define T_FW 0b01100111 // float widen
-#define T_FN 0b01100111 // float narrow
+#define T_A   0b11111111 // all
+#define T_W   0b01110111 // widen
+#define T_WR  0b01111111 // widen reduction
+#define T_N   0b01110111 // narrow
+#define T_F   0b11101111 // float
+#define T_FW  0b01100111 // float widen
+#define T_FWR 0b01101111 // float widen reduction
+#define T_FN  0b01100111 // float narrow
 
 #define T_E2 0b11101110 // extend 2
 #define T_E4 0b11001100 // extend 4
