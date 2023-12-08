7cccabac9c00b6778321ca5c716d4dca4e9be1d2
Olaf Bernstein <camel-cdr@protonmail.com>
2023-12-08 23:04:28 +0100

    stick with rv64gcv as default config

--- config.mk
+++ config.mk
@@ -3,11 +3,11 @@ WARN=-Wall -Wextra -Wno-unused-function -Wno-unused-parameter
 
 # freestanding using any recent clang build
 CC=clang
-CFLAGS=--target=riscv64 -march=rv64gcv_zfh -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
+CFLAGS=--target=riscv64 -march=rv64gcv -O3 ${WARN} -nostdlib -fno-builtin -ffreestanding
 
 # full cross compilation toolchain
 #CC=riscv64-linux-gnu-gcc
-#CFLAGS=-march=rv64gcv_zfh -O3 ${WARN}
+#CFLAGS=-march=rv64gcv -O3 ${WARN}
 
 # native build
 #CC=cc
