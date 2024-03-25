b55625da79033e4e12ee7baa1034a87cd6faa616
Olaf Bernstein <camel-cdr@protonmail.com>
2024-03-26 00:11:32 +0100

    add proper extension detection

--- instructions/rv64/config.h
+++ instructions/rv64/config.h
@@ -3,15 +3,3 @@
 #define LOOP 16
 #define RUNS 1000
 
-/* processor specific configs */
-
-#define HAS_M __riscv_m
-#define HAS_A __riscv_a
-#define HAS_F __riscv_f
-#define HAS_D __riscv_d
-
-#define HAS_Zba 1
-#define HAS_Zbb 1
-#define HAS_Zbc 0
-#define HAS_Zbs 1
-
