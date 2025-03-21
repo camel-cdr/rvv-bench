96654ffab81c8534cb823f83adc0ed28170e3c5e
Olaf Bernstein <camel-cdr@protonmail.com>
2025-03-21 11:55:48 +0100

    fix base64-encode for RV32

--- bench/base64-encode.c
+++ bench/base64-encode.c
@@ -1,45 +1,49 @@
 #include "bench.h"
 
 size_t
-b64_encode_scalar(uint8_t *dst, const uint8_t *src, size_t length, const uint8_t *lut, const uint8_t *shift)
+b64_encode_scalar(uint8_t *dst, const uint8_t *src, size_t length, const uint8_t LUTs[64+16])
 {
 	uint8_t *dstBeg = dst;
 	for (; length >= 3; length -= 3, src += 3, dst += 4) {
 		uint32_t u32 = src[0] << 16 | src[1] << 8 | src[2];
-		dst[0] = lut[(u32 >> 18) & 63];
-		dst[1] = lut[(u32 >> 12) & 63];
-		dst[2] = lut[(u32 >>  6) & 63];
-		dst[3] = lut[(u32 >>  0) & 63];
+		dst[0] = LUTs[(u32 >> 18) & 63];
+		dst[1] = LUTs[(u32 >> 12) & 63];
+		dst[2] = LUTs[(u32 >>  6) & 63];
+		dst[3] = LUTs[(u32 >>  0) & 63];
 	}
 	if (length > 0) {
 		uint32_t u32 = src[0] << 8 | (length > 1 ? src[1] : 0);
-		*dst++ =              lut[(u32 >> 10) & 63];
-		*dst++ =              lut[(u32 >> 4) & 63];
-		*dst++ = length > 1 ? lut[(u32 << 2) & 63] : '=';
-		*dst++ =                                     '=';
+		*dst++ =              LUTs[(u32 >> 10) & 63];
+		*dst++ =              LUTs[(u32 >>  4) & 63];
+		*dst++ = length > 1 ? LUTs[(u32 <<  2) & 63] : '=';
+		*dst++ =                                       '=';
 	}
 	return dst - dstBeg;
 }
 
-static uint8_t base64LUT[] =
+static uint8_t base64LUTs[64 + 16] =
 	"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 	"abcdefghijklmnopqrstuvwxyz"
 	"0123456789"
-	"+/";
+	"+/"
+	"\x47\xfc\xfc\xfc\xfc\xfc\xfc\xfc\xfc\xfc\xfc\xed\xf0\x41"
+	// 'a'-26, 10x '0' - 52, '+' - 62, '/' - 63, 'A'
+;
 
-static const uint8_t base64shiftLUT[16] = {
-	'a' - 26, '0' - 52, '0' - 52, '0' - 52,
-	'0' - 52, '0' - 52, '0' - 52, '0' - 52,
-	'0' - 52, '0' - 52, '0' - 52, '+' - 62,
-	'/' - 63, 'A'
-};
+/* used to prevent GPR spill in vectorized implementations */
+size_t
+b64_encode_scalar_tail(size_t prefix, uint8_t *dst, const uint8_t *src, size_t length)
+{
+	return prefix + b64_encode_scalar(dst, src, length, base64LUTs);
+}
+
 
 #define IMPLS(f) \
 	f(scalar) \
 	f(rvv_LUT64) f(rvv_LUT16) \
 	f(rvv_seg_LUT64) f(rvv_seg_LUT16)
 
-typedef size_t Func(uint8_t *dst, const uint8_t *src, size_t length, const uint8_t *lut, const uint8_t *shift);
+typedef size_t Func(uint8_t *dst, const uint8_t *src, size_t length, const uint8_t LUTs[64+16]);
 
 #define DECLARE(f) extern Func b64_encode_##f;
 IMPLS(DECLARE)
@@ -48,13 +52,13 @@ IMPLS(DECLARE)
 Impl impls[] = { IMPLS(EXTRACT) };
 
 uint8_t *dest, *src;
-ux last;
+size_t last;
 
 void init(void) { }
 
 ux checksum(size_t n) {
 	ux sum = last;
-	for (size_t i = 0; i < n+9; ++i)
+	for (size_t i = 0; i < last+9; ++i)
 		sum = uhash(sum) + dest[i];
 	return sum;
 }
@@ -62,8 +66,8 @@ ux checksum(size_t n) {
 BENCH_BEG(base) {
 	src = mem;
 	dest = mem + MAX_MEM/3;
-	memset(dest, 0, n+9);
-	TIME last = (uintptr_t)f(dest, src, n, base64LUT, base64shiftLUT);
+	memset(dest, 0, n*2+9);
+	TIME last = f(dest, src, n, base64LUTs);
 } BENCH_END
 
 Bench benches[] = {
