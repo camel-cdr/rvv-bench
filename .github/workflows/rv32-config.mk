a9fa15f59973b9122c3b8a70b9b37db99b3801c1
Olaf Bernstein <camel-cdr@protonmail.com>
2026-06-11 09:15:11 +0200

    update CI

--- .github/workflows/rv32-config.mk
+++ .github/workflows/rv32-config.mk
@@ -1,3 +1,3 @@
 WARN=-Wall -Wextra -Wno-unused-function -Wno-unused-parameter
-CC=clang-17
+CC=clang-20
 CFLAGS=--target=riscv32 -march=rv32gc_zve32f_zfh_zba_zbb_zbs -O3 ${WARN} -nostdlib -fno-builtin -nodefaultlibs -ffreestanding
--- .github/workflows/rv64-run.sh
+++ .github/workflows/rv64-run.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-# zfh=true,zvfh=true disabled for now
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=128,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=256,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=512,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=1024,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=128,rvv_ta_all_1s=off,rvv_ma_all_1s=off $@ && \
-qemu-riscv64-static -cpu rv64,v=on,vext_spec=v1.0,vlen=1024,rvv_ta_all_1s=off,rvv_ma_all_1s=off $@
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=128,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=256,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=512,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=1024,rvv_ta_all_1s=on,rvv_ma_all_1s=on $@ && \
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=128,rvv_ta_all_1s=off,rvv_ma_all_1s=off $@ && \
+qemu-riscv64-static -cpu rv64,v=on,zfh=true,vext_spec=v1.0,vlen=1024,rvv_ta_all_1s=off,rvv_ma_all_1s=off $@
